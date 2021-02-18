af14add8a684536c22461e38ca2cd3c2bab07284
FurFinance <72808856+FurFinance@users.noreply.github.com>
2021-02-18 15:05:18 -0500

    Update constants.js

--- src/sushi/lib/constants.js
+++ src/sushi/lib/constants.js
@@ -485,20 +485,7 @@ export const contractAddresses = {
 
 
 export const supportedPools = [
-  {
-    pid: 0,
-    lpAddresses: {
-      1: '0x838cC44eb793d79dd4D33D8CbE9b58A1556626D4', // LP Token Address
-    },
-    tokenAddresses: {
-      1: '0xbacdbe7cd52bdf11065210863139e9dad875a896', // "FUR"
-      
-    },
-    name: 'Looking Fur Group',
-    symbol: 'FUR-ETH LP',
-    tokenSymbol: 'DAI',
-    icon: <img src={catImg} width="78" height="78"/>
-  },
+  
   {
     pid: 1,
     lpAddresses: {
@@ -568,6 +555,20 @@ export const supportedPools = [
     symbol: 'USDC-ETH LP',
     tokenSymbol: 'DAI',
     icon: <img src={can} width="70" height="70"/>
+  },
+   {
+    pid: 6,
+    lpAddresses: {
+      1: '0x838cC44eb793d79dd4D33D8CbE9b58A1556626D4', // LP Token Address
+    },
+    tokenAddresses: {
+      1: '0xbacdbe7cd52bdf11065210863139e9dad875a896', // "FUR"
+      
+    },
+    name: 'Looking Fur Group',
+    symbol: 'FUR-ETH LP',
+    tokenSymbol: 'DAI',
+    icon: <img src={catImg} width="78" height="78"/>
   },
 ]
 
